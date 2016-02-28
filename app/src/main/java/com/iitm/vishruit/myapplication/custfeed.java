a9eda2b51779a97c2f375c96a7e4993951dda1bc
Vishruit <princepaulite@gmail.com>
2016-02-28 06:31:11 +0530

    v0

--- app/src/main/java/com/iitm/vishruit/myapplication/custfeed.java
+++ app/src/main/java/com/iitm/vishruit/myapplication/custfeed.java
@@ -1,5 +1,6 @@
 package com.iitm.vishruit.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,14 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class custfeed extends AppCompatActivity {
+public class custfeed extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custfeed);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
