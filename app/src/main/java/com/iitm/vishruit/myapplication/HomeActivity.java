c1a2fbff69d05f473c8ad896e1fa78cc3848f7a8
dharanimanne <dharani.manne@gmail.com>
2016-02-28 06:51:02 +0530

    main activity skeleton done

--- app/src/main/java/com/iitm/vishruit/myapplication/HomeActivity.java
+++ app/src/main/java/com/iitm/vishruit/myapplication/HomeActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class custfeed extends Activity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/iitm/vishruit/myapplication/MainActivity.java
+++ app/src/main/java/com/iitm/vishruit/myapplication/MainActivity.java
@@ -1,13 +1,26 @@
 package com.iitm.vishruit.myapplication;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.util.Arrays;
+import com.iitm.vishruit.myapplication.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final SharedPreferences sharedPrefLogin = getSharedPreferences(getString(R.string.sharedPref), Context.MODE_PRIVATE);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +41,38 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button loginButton = (Button) findViewById(R.id.button_login);
+
+        if (sharedPrefLogin.contains("LoginStatus")){
+            Log.d("Log : ", "LoginStatus True");
+            // Share URI Data
+            Uri data = getIntent().getData();
+            if(data != null){
+                // Redirect to Home Page
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                homeIntent.putExtra("URIData", data); // Pass DATA URI
+                startActivity(homeIntent);
+                finish(); // call this to finish the current activity. Preventing Back buttton.
+            } else {
+//                Log.d("Log : ", "No URI Data Found");
+                // Redirect to Home Page
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(homeIntent);
+                finish(); // call this to finish the current activity. Preventing Back buttton.
+            }
+        } else {
+            Log.d("Log : ", "Does not Contain LoginStatus");
+            loginButton.setOnClickListener(new Button.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    //Do something when someone clicks on submit button...send a http request and handle it.
+                }
+            });
+
+        }
     }
 
     @Override
@@ -49,4 +96,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
