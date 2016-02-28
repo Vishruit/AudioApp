f9d290ee3f4d2c71a980066ae0c3c877e165b2ba
dharanimanne <dharani.manne@gmail.com>
2016-02-28 07:30:06 +0530

    works

--- app/src/main/java/com/iitm/vishruit/myapplication/feed_comment.java
+++ app/src/main/java/com/iitm/vishruit/myapplication/feed_comment.java
@@ -15,15 +15,6 @@ public class feed_comment extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_comment);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
